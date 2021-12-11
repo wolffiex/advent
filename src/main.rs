64d70361de44a33ee2e9dbf389181d8aefe2fc26
Adam Wolff <adam@electra.localdomain>
2021-12-11 14:18:15 -0800

    Man that was hard

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::collections::HashSet;
 
 type Grid = Vec<Vec<u32>>;
 
-#[derive(PartialEq, Eq, Hash)]
-struct Point(usize, usize);
+#[derive(Clone, PartialEq, Eq, Hash, Copy)]
+struct Point {
+    x: usize,
+    y: usize,
+}
 
 fn main() {
     fn convert_line(line: &str) -> Vec<u32> {
@@ -14,33 +17,93 @@ fn main() {
     }
 
     let split = get_input().split("\n");
-    let initial: Grid = split.into_iter().map(convert_line).collect();
-    let next = step(initial);
+    let mut grid: Grid = split.into_iter().map(convert_line).collect();
+    // print_grid(&grid);
+    let mut total = 0;
+    for s in 1..=100 {
+        let (next, flashes) = step(&grid);
+        total = total + flashes.len();
+        grid = next;
+        // println!("step {}: {}", s, flashes.len());
+        print_grid(&grid);
+    }
+    println!("pp : {}", total);
 }
 
-fn step(current: Grid) -> Grid {
+fn step(current: &Grid) -> (Grid, HashSet<Point>) {
     //increment
     let mut next: Grid = current.into_iter().map(|row| -> Vec<u32>{
         row.into_iter().map(|n| -> u32{ n + 1 }).collect()
     }).collect();
 
-    let mut flashed: HashSet<Point> = HashSet::new();
+    let width = next.get(0).unwrap().len();
+    let mut all_flashed: HashSet<Point> = HashSet::new();
 
     loop {
         let mut pass_flashed: HashSet<Point> = HashSet::new();
         for y in 0..next.len() {
-            let row = next.get(y).unwrap();
-            for x in 0..row.len() {
-                let val = row.get(x).unwrap();
+            for x in 0..width {
+                let p = Point { x, y };
+                let v = next[y][x];
+                if v > 9u32 && !all_flashed.contains(&p) {
+                    pass_flashed.insert(p);
+                }
             }
         }
 
-        if pass_flashed.len() == 0 { break; }
+        if pass_flashed.len() > 0 {
+            for pf in pass_flashed {
+                for dx in -1i8..=1 {
+                    for dy in -1i8..=1 {
+                        let op = translate_point(pf, dx, dy);
+                        if let Some(p) = op {
+                            if let Some(row) = next.get_mut(p.y) {
+                                if let Some(v) = row.get_mut(p.x) {
+                                    *v = *v + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                all_flashed.insert(pf);
+            }
+        } else {
+            for p in &all_flashed {
+                *next.get_mut(p.y).unwrap().get_mut(p.x).unwrap() = 0;
+            }
+            break;
+        }
     }
-    return next;
+    return (next, all_flashed);
+}
+
+fn translate_point(p: Point, dx: i8, dy: i8) -> Option<Point> {
+    fn positive_sum(u: usize, du: i8) -> Option<usize> {
+        let nu = u as isize + du as isize;
+        return if nu < 0 { None } else { Some(nu as usize) };
+    }
+    return Some(Point { x: positive_sum(p.x, dx)?, y: positive_sum(p.y, dy)? });
+}
+
+fn print_grid(grid: &Grid) {
+    for row in grid {
+        println!("{:?}", row);
+    }
 }
 
 fn get_input() -> &'static str {
+    return "6318185732
+1122687135
+5173237676
+8754362612
+5718474666
+8443654137
+1247634346
+1446514585
+6717288267
+1727871228";
+}
+fn xget_input() -> &'static str {
     return "5483143223
 2745854711
 5264556173
