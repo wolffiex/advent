b4e778899e21cedcc800f107f1828583c99caf44
Adam Wolff <adam@electra.local>
2021-12-12 08:30:02 -0800

    Day 11 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,67 @@
 #![allow(dead_code)]
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-
-type Grid = Vec<Vec<u32>>;
-
-#[derive(Clone, PartialEq, Eq, Hash, Copy)]
-struct Point {
-    x: usize,
-    y: usize,
-}
 
 fn main() {
-    fn convert_line(line: &str) -> Vec<u32> {
-        return line.chars().map(|c| -> u32{ c.to_digit(10).unwrap() }).collect();
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in get_input().split("\n") {
+        let mut nodes = line.split("-");
+        let a = nodes.next().unwrap();
+        let b = nodes.next().unwrap();
+        map.entry(a).or_default().push(b);
+        map.entry(b).or_default().push(a);
     }
-
-    let split = get_input().split("\n");
-    let mut grid: Grid = split.into_iter().map(convert_line).collect();
-    let size = grid.len() * grid[0].len();
-    // print_grid(&grid);
-    let mut total = 0;
-    let mut step_count = 0;
+    let mut stack:Vec<Vec<&str>> = vec!{vec!{"start"}};
+    let mut paths_through:Vec<Vec<&str>> = Vec::new();
     loop {
-        let (next, flashes) = step(&grid);
-        step_count = step_count + 1;
-        total = total + flashes.len();
-        grid = next;
-        if flashes.len() == size {
-            println!("step {}: {}", step_count, flashes.len());
-            print_grid(&grid);
-            break;
-        }
-    }
-    println!("pp : {}", total);
-}
-
-fn step(current: &Grid) -> (Grid, HashSet<Point>) {
-    //increment
-    let mut next: Grid = current.into_iter().map(|row| -> Vec<u32>{
-        row.into_iter().map(|n| -> u32{ n + 1 }).collect()
-    }).collect();
-
-    let width = next.get(0).unwrap().len();
-    let mut all_flashed: HashSet<Point> = HashSet::new();
-
-    loop {
-        let mut pass_flashed: HashSet<Point> = HashSet::new();
-        for y in 0..next.len() {
-            for x in 0..width {
-                let p = Point { x, y };
-                let v = next[y][x];
-                if v > 9u32 && !all_flashed.contains(&p) {
-                    pass_flashed.insert(p);
+        let path = match stack.pop() {
+            Some(p) => p,
+            None => break,
+        };
+        let path_end = path.last().unwrap();
+        println!("want to get : {:?}", path_end);
+        let next_nodes = map.get(path_end).unwrap();
+        for node in next_nodes {
+            let is_big = node.chars().next().unwrap().is_uppercase();
+            if is_big || !path.contains(node) {
+                let mut new_path = path.clone();
+                new_path.push(node);
+                println!("path: {:?}", new_path);
+                if node.contains("end") {
+                    paths_through.push(new_path);
+                } else {
+                    stack.push(new_path);
                 }
+                println!("stack: {:?}", stack);
             }
         }
-
-        if pass_flashed.len() > 0 {
-            for pf in pass_flashed {
-                for dx in -1i8..=1 {
-                    for dy in -1i8..=1 {
-                        let op = translate_point(pf, dx, dy);
-                        if let Some(p) = op {
-                            if let Some(row) = next.get_mut(p.y) {
-                                if let Some(v) = row.get_mut(p.x) {
-                                    *v = *v + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                all_flashed.insert(pf);
-            }
-        } else {
-            for p in &all_flashed {
-                *next.get_mut(p.y).unwrap().get_mut(p.x).unwrap() = 0;
-            }
-            break;
-        }
     }
-    return (next, all_flashed);
-}
-
-fn translate_point(p: Point, dx: i8, dy: i8) -> Option<Point> {
-    fn positive_sum(u: usize, du: i8) -> Option<usize> {
-        let nu = u as isize + du as isize;
-        return if nu < 0 { None } else { Some(nu as usize) };
-    }
-    return Some(Point { x: positive_sum(p.x, dx)?, y: positive_sum(p.y, dy)? });
-}
-
-fn print_grid(grid: &Grid) {
-    for row in grid {
-        println!("{:?}", row);
-    }
+    println!("HO: {:?}", paths_through.len());
 }
 
 fn get_input() -> &'static str {
-    return "6318185732
-1122687135
-5173237676
-8754362612
-5718474666
-8443654137
-1247634346
-1446514585
-6717288267
-1727871228";
-}
-fn xget_input() -> &'static str {
-    return "5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526";
+    return "vn-DD
+qm-DD
+MV-xy
+end-xy
+KG-end
+end-kw
+qm-xy
+start-vn
+MV-vn
+vn-ko
+lj-KG
+DD-xy
+lj-kh
+lj-MV
+ko-MV
+kw-qm
+qm-MV
+lj-kw
+VH-lj
+ko-qm
+ko-start
+MV-start
+DD-ko"
 }
