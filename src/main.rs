dbeb85539cfd058a1cc3c993aafb394bbe6495d8
Adam Wolff <adam@electra.local>
2021-12-14 10:19:05 -0800

    Correct length

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,40 @@ fn main() {
 struct Pair(char, char);
 
 fn part1() -> Option<()> {
-    let (initial, rules) = get_input().split_once("\n\n")?;
-    let rule_map: HashMap<Pair, char> = rules.lines().filter_map(
-        |line: &str| -> Option<(Pair, _)> {
-            Some({
-                let (a, b) = line.split_once(" -> ")?;
-                let pair = head_pair(a.into());
-                (pair?, b.chars().next()?)
-            })
-        }
-    ).collect();
-    let mut sequence:String = initial.into();
-    for i in 0..10 {
-        sequence = step(sequence, &rule_map);
+    let (mut sequence, rule_map) = process_input(get_input())?;
+    for _ in 0..10 {
+        sequence = step(sequence, &rule_map)?;
     }
+    println!("{}", sequence.len());
     return Some(());
 }
 
-fn head_pair(s:String) -> Option<Pair>{
-    if let [a, b, ..] = &s[0..2] {
-        return Some(Pair(a,b));
-    }
-    None
+fn step(sequence: String, rules: &HashMap<Pair, char>) -> Option<String> {
+    let mut it = sequence.chars();
+    let last_char = it.next()?;
+    let next_step = it.map(|c| -> String {
+        let new_char = rules.get(&Pair(last_char, c)).unwrap();
+        format!("{}{}", last_char, new_char)
+    }).collect::<String>();
+    return Some(format!("{}{}", sequence.chars().last()?, next_step));
 }
-fn step(sequence:String, rules:&HashMap<Pair, char>) -> String {
-    return  String::from("dldkfjd");
+
+fn process_input(input: &str) -> Option<(String, HashMap<Pair, char>)> {
+    let (sequence, rules) = input.split_once("\n\n")?;
+    return Some((sequence.into(), rules.lines().filter_map(
+        |line: &str| -> Option<(Pair, _)> {
+            Some({
+                let (a, b) = line.split_once(" -> ")?;
+                let pair = head_pair(a);
+                (pair?, b.chars().next()?)
+            })
+        }
+    ).collect()));
+}
+
+fn head_pair(s: &str) -> Option<Pair> {
+    let mut it = s.chars();
+    Some(Pair(it.next()?, it.next()?))
 }
 
 fn get_input() -> &'static str {
