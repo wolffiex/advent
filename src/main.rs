9cfafbdc9ded63a3cea2b7778f697c78642c4596
Adam Wolff <adam@electra>
2021-12-19 14:17:55 -0800

    Magnitude OMG

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,125 @@ fn parse_number(s: &str) -> Vec<Elem> {
     }).collect()
 }
 
+
+fn split(starting: &Vec<Elem>) -> Option<Vec<Elem>> {
+    let mut did_one = false;
+    let after_split: Vec<Elem> = starting.into_iter().flat_map(|e| -> Vec<Elem> {
+        if let Num(v) = e {
+            if !did_one && *v > 9 {
+                did_one = true;
+                let rounded_up = (v + (2 - 1)) / 2;
+                return vec![Open, Num(v / 2), Num(rounded_up), Close];
+            }
+        }
+        vec![*e]
+    }).collect();
+
+    match did_one {
+        false => None,
+        true => Some(after_split),
+    }
+}
+
+fn add(a: &Vec<Elem>, b: &Vec<Elem>) -> Vec<Elem> {
+    let mut result: Vec<Elem> =
+        [slice::from_ref(&Open), a.as_slice(), b.as_slice(), slice::from_ref(&Close)].concat();
+    loop {
+        result = match explode(&result) {
+            None => match split(&result) {
+                None => break,
+                Some(new_result) => new_result,
+            }
+            Some(new_result) => new_result,
+        }
+    }
+    result
+}
+
+#[test]
+fn test_add() {
+    let a = "[[[[4,3],4],4],[7,[[8,4],9]]]";
+    let b = "[1,1]";
+    let result = add(&parse_number(a), &parse_number(b));
+    assert!(equiv(result, parse_number("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")));
+}
+
+#[test]
+fn test_example() {
+    let sam = "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]";
+    assert!(equiv(add_list(sam), parse_number("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")));
+}
+
+#[test]
+fn test_example2() {
+    let sam = "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
+    assert!(equiv(add_list(sam),
+                  parse_number("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]")));
+}
+
+fn magnitude(num: Vec<Elem>) -> usize {
+    if num.len() == 1 {
+        return match num.get(0) {
+            Some(Num(n)) => *n,
+            _ => panic!("Bad number"),
+        };
+    }
+
+    let num_iter = num.into_iter();
+    let take_one = |skip_count: usize| -> Vec<Elem>  {
+        let mut depth = 0;
+        let mut did_one = false;
+        num_iter.clone().skip(skip_count).take_while(|e: &Elem| -> bool {
+            let old_depth = depth;
+            depth = match e {
+                Open => depth + 1,
+                Close => depth - 1,
+                _ => depth,
+            };
+            let now_stop = old_depth == 0 && did_one;
+            did_one = true;
+            !now_stop
+        }).collect()
+    };
+    let left = take_one(1);
+    let right = take_one(1 + left.len());
+    return 3 * magnitude(left) + 2 * magnitude(right);
+}
+
+#[test]
+fn test_magnitude() {
+    assert_eq!(magnitude(parse_number("[[1,2],[[3,4],5]]")), 143);
+}
+
+fn add_list(input: &str) -> Vec<Elem> {
+    input.lines().map(parse_number).fold(
+        None,
+        |prev: Option<Vec<Elem>>, num: Vec<Elem>| -> Option<Vec<Elem>> {
+            match prev {
+                Some(prev_num) => Some(add(&prev_num, &num)),
+                None => Some(num),
+            }
+        }).unwrap()
+}
+
 fn explode(starting: &Vec<Elem>) -> Option<Vec<Elem>> {
     let mut depth = 0;
     let maybe_pos = starting.into_iter().enumerate().find_map(|(p, e): (usize, &Elem)| -> Option<usize> {
@@ -56,12 +175,12 @@ fn explode(starting: &Vec<Elem>) -> Option<Vec<Elem>> {
         Some(p) => {
             let (left, right): (Elem, Elem) = (*starting.get(p)?, *starting.get(p + 1)?);
             let mut did_left = false;
-            let foo:Vec<Elem> = starting.into_iter().take(p).rev().map(|e| *e).collect();
+            let foo: Vec<Elem> = starting.into_iter().take(p).rev().map(|e| *e).collect();
             let mut head: Vec<Elem> = starting.into_iter().take(p - 1).rev().map(|e| -> Elem {
                 if let Num(_) = e {
                     if !did_left {
                         did_left = true;
-                        return plus(*e, left);
+                        return plus_elems(*e, left);
                     }
                 }
                 *e
@@ -73,7 +192,7 @@ fn explode(starting: &Vec<Elem>) -> Option<Vec<Elem>> {
                 if let Num(_) = p {
                     if !did_right {
                         did_right = true;
-                        return plus(*p, right);
+                        return plus_elems(*p, right);
                     }
                 }
                 *p
@@ -84,7 +203,7 @@ fn explode(starting: &Vec<Elem>) -> Option<Vec<Elem>> {
     }
 }
 
-fn plus(a: Elem, b: Elem) -> Elem {
+fn plus_elems(a: Elem, b: Elem) -> Elem {
     match (a, b) {
         (Num(na), Num(nb)) => Num(na + nb),
         _ => panic!("Can't plus")
